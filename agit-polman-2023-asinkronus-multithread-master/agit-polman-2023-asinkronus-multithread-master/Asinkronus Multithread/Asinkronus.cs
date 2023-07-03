605b46139447e2b72b524c7110bce4155c962c40
bogelegob <137293464+bogelegob@users.noreply.github.com>
2023-07-03 22:15:24 +0700

    Add files via upload

---
+++ agit-polman-2023-asinkronus-multithread-master/agit-polman-2023-asinkronus-multithread-master/Asinkronus Multithread/Asinkronus.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asinkronus_Multithread
+{
+    internal class Asinkronus
+    {
+        public static async Task FirstAsync()
+        {
+            Console.WriteLine("First Async Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(1000);
+            Console.WriteLine("First Async Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        public static async Task SecondAsync()
+        {
+            Console.WriteLine("Second Async Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(1000);
+            Console.WriteLine("Second Async Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        public static async Task ThirdAsync()
+        {
+            Console.WriteLine("Third Async Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(1000);
+            Console.WriteLine("Third Async Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        public static async Task ForthAsync()
+        {
+            Console.WriteLine("Jurusan Saya ");
+            await Task.Delay(3000);
+            Console.WriteLine("IPA");
+        }
+
+        public static async Task FifthhAsync()
+        {
+            Console.WriteLine("Nama Saya ");
+            await Task.Delay(2000);
+            Console.WriteLine("Farras Abdillah");
+        }
+        public static async Task ExecuteAsyncFunctions()
+        {
+            var firstAsync = FirstAsync();
+            var secondAsync = SecondAsync();
+            var thirdAsync = ThirdAsync();
+            var forthAsync = ForthAsync();
+            var fifthAsync = FifthhAsync();
+            await Task.WhenAll(firstAsync, secondAsync, thirdAsync, forthAsync, fifthAsync);
+        }
+    }
+}
---
+++ agit-polman-2023-asinkronus-multithread-master/agit-polman-2023-asinkronus-multithread-master/Asinkronus Multithread/Multithreading.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asinkronus_Multithread
+{
+    internal class Multithreading
+    {
+        public static void FirstMethod()
+        {
+            Console.WriteLine("First Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(1000);
+            Console.WriteLine("First Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        public static void SecondMethod()
+        {
+            Console.WriteLine("Second Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(1000);
+            Console.WriteLine("Second Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        public static void ThirdMethod()
+        {
+            Console.WriteLine("Third Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(1000);
+            Console.WriteLine("Third Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        public static void ForthMethod()
+        {
+            Console.WriteLine("Jurusan Saya ");
+            Thread.Sleep(2000);
+            Console.WriteLine("IPA");
+        }
+
+        public static void FifthMethod()
+        {
+            Console.WriteLine("Nama Saya ");
+            Thread.Sleep(1500);
+            Console.WriteLine("Farras Abdillah");
+        }
+        public static void ExecuteMultithreading()
+        {
+            Thread t1 = new Thread(new ThreadStart(FirstMethod));
+            Thread t2 = new Thread(new ThreadStart(SecondMethod));
+            Thread t3 = new Thread(new ThreadStart(ThirdMethod));
+            Thread t4 = new Thread(new ThreadStart(ForthMethod));
+            Thread t5 = new Thread(new ThreadStart(FifthMethod));
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+            t5.Start();
+        }
+    }
+}
